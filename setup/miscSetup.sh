cc732ecc8b120f92b3971260acac1e0c0aae7fec
dkilcy <david@kilcyconsulting.com>
2013-12-10 14:26:26 -0500

    Add more packages

--- setup/miscSetup.sh
+++ setup/miscSetup.sh
@@ -25,12 +25,23 @@ echo "Start of program"
 
 cd /data/software
 
+yum install yum-plugin-priorities
+
 ##wget http://pkgs.repoforge.org/lshw/lshw-2.15-1.el6.rf.x86_64.rpm
 ##rpm -ivh lshw-2.15-1.el6.rf.x86_64.rpm 
+
+yum install perl
+yum install git-1.7.12.4-1.el6.rfx.x86_64.rpm perl-Git-1.7.12.4-1.el6.rfx.x86_64.rpm
 
 yum install gcalctool
 yum install gedit
 yum install p7zip
+
+yum install gnome-system-monitor
+yum install libreoffice
+yum install build-essential
+yum install gcc
+yum install g++
 
 ##yum whatprovides /usr/sbin/semanage
 yum install policycoreutils-python
@@ -39,6 +50,8 @@ yum install policycoreutils-gui
 rpm -ivh epel-release-6-8.noarch.rpm
 yum install nodejs
 yum install npm
+
+yum install mongo-10gen mongo-10gen-server
 
 rpm -ivh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 
